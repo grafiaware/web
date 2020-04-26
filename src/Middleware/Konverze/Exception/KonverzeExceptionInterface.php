43c7eae7ef37ef6bd6a3f3fa0c617f3859fa1f15
Petr Svoboda <svoboda@grafia.cz>
2020-04-26 20:47:39 +0200

    konverze_exceptions

---
+++ src/Middleware/Konverze/Exception/KonverzeExceptionInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Middleware\Konverze\Exception;
+
+/**
+ *
+ * @author pes2704
+ */
+interface KonverzeExceptionInterface {
+    //put your code here
+}
---
+++ src/Middleware/Konverze/Exception/MaxExucutionTimeException.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Middleware\Konverze\Exception;
+
+/**
+ * Description of MaxExucutionTimeException
+ *
+ * @author pes2704
+ */
+class MaxExucutionTimeException extends \RuntimeException implements KonverzeExceptionInterface {
+    //put your code here
+}
