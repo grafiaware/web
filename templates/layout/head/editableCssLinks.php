5d039f3c64e78ab47eaf7d2b1544fecd3043a81f
Kateřina Šlehoferová <slehoferova@grafia.cz>
2020-09-08 09:41:31 +0200

    rozdeleni stylu pro normalni a editacni rezim

--- templates/layout/head/editableCssLinks.php
+++ templates/layout/head/editableCssLinks.php
@@ -1,1 +1,3 @@
-<link rel="stylesheet" type="text/css" href="<?= $webPublicDir ?>grafia/css/novestylyodkacky.css">
+<link rel="stylesheet" type="text/css" href="<?= $webPublicDir ?>grafia/css/prvky_editace.css">
+<link rel="stylesheet" type="text/css" href="<?= $webPublicDir ?>grafia/css/editor.css">
+<link rel="stylesheet" type="text/css" href="<?= $webPublicDir ?>grafia/css/template.css">
