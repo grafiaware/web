5addd73096d11bc13a00098daba93355e2966fb6
Petr Svoboda <svoboda@grafia.cz>
2020-11-05 07:44:51 +0100

    block_sql

--- src/Middleware/Build/Sql/page5_insertIntoMenuRoot&ComponentTable.sql
+++ src/Middleware/Build/Sql/page5_insertIntoMenuRoot&ComponentTable.sql
@@ -8,17 +8,10 @@ WHERE list='root' OR  list='trash' OR  list='blocks' OR  list='menu_vertical' OR
 GROUP BY uid_fk;
 
 -- naplnění tabulky block
-  -- potomky kořenové položky 'a' (tj potomky a)
+  --  položky 'a'
 INSERT INTO block (uid_fk, name)
-SELECT menu_item.uid_fk, menu_item.list FROM
-menu_item
-INNER JOIN
-(SELECT
-list
+SELECT
+menu_item.uid_fk, menu_item.list
 FROM menu_item
-WHERE list='a'
-) AS comp ON (
-    LENGTH(menu_item.list)>LENGTH(comp.list)
-    AND SUBSTR(menu_item.list, 1, 1)=comp.list
-    )
+WHERE SUBSTR(menu_item.list, 1, 1)='a'
 GROUP BY uid_fk
