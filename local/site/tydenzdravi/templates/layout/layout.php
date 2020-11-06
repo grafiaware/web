892d4ac10575223d378db628106977941c084447
Petr Svoboda <svoboda@grafia.cz>
2020-11-06 19:11:12 +0100

    tz_fix

--- local/site/tydenzdravi/templates/layout/layout.php
+++ local/site/tydenzdravi/templates/layout/layout.php
@@ -4,20 +4,12 @@
         <base href="<?= $basePath; ?>">
         <title><?= $title; ?></title>
         <?php include "head/meta.php"; ?>
-        <?php include "head/linkCss.php"; ?>
-        <?php include "head/linkJs.php"; ?>
+        <?php include "head/links.php"; ?>
+        <?php include "head/scripts.php"; ?>
     </head>
     <body class="layoutTZ">
         <?php include "body/container.php"; ?>
         <?php include "body/scripts.php"; ?>
-        <script>
-            $('.ui.dropdown')
-              .dropdown()
-            ;
-            $('.ui.selection.dropdown')
-              .dropdown()
-            ;
-        </script>
     </body>
 </html>
 
