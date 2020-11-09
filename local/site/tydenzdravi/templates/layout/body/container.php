ea984b5678f730aa490623f87c784ee6e1281449
Kateřina Šlehoferová <slehoferova@grafia.cz>
2020-11-09 09:26:28 +0100

    Media query_fáze1

--- local/site/tydenzdravi/templates/layout/body/container.php
+++ local/site/tydenzdravi/templates/layout/body/container.php
@@ -1,20 +1,20 @@
     <!--<div class="ui container editable">--> <!--$this->attributes($bodyContainerAttributes) -->
     <div class="ui grid">
-        <div class="three wide tablet four wide computer column">
+        <div class="one wide tablet two wide computer four wide large screen four wide widescreen column">
             <div class="fix-bar">
                 <?php include "container/telo/svislemenu.php"; ?>
                 <?php include "container/hlavicka/prihlaseni.php"; ?>
                 <?php include "container/telo/iconmenu.php"; ?>
             </div>
         </div>
-        <div class="twelve wide tablet ten wide computer column">
+        <div class="fifteen wide tablet thirteen wide computer ten wide large screen ten wide widescreen column">
             <header>
                 <?php include "container/hlavicka.php"; ?>
             </header>
             <main class="page-content">
                 <?= $flash ?? '' ?>
                 <?= $poznamky ?? '' ?>
-                <?php include "container/telo_1.php"; ?>
+                <?php include "container/telo_tiny.php"; ?>
             </main>
         </div>
     </div>
