1a399b4d4061587700494e5137f45773f5428c12
Kateřina Šlehoferová <slehoferova@grafia.cz>
2020-11-13 18:36:37 +0100

    pro-media a logo-ke-stazeni

---
+++ local/site/tydenzdravi/templates/static/content/content_LogoKeStazeni.php
@@ -0,0 +1,29 @@
+<?php
+use Pes\View\Renderer\PhpTemplateRendererInterface;
+use Model\Entity\PaperAggregateInterface;
+/** @var PhpTemplateRendererInterface $this */
+/** @var PaperAggregateInterface $paperAggregate */
+?>
+
+<div class="logo-ke-stazeni">
+    <p class="primarni-barva nadpis">Logo ke stažení</p>
+    <p class="text"></p>
+    <div class="ui centered grid">
+            <div class="sixteen wide column">
+                <p><img src="images/LogoTZ.jpg" alt="Logo Týden zdraví" width="362" height="130" /></p>
+                <p><a href="files/LOGO_Týden_zdraví.ai" target="_blank">Logo ve formátu AI</a></p>
+                <p><a href="files/LOGO_Týden_zdraví_2.png" target="_blank">Logo ve formátu PNG</a></p>
+                <p><a href="files/LOGO_Týden_zdraví_JPG_1.jpg" target="_blank">Logo ve formátu JPG</a></p>
+                
+            </div>
+        <p class="text"></p>
+        <p class="text"></p>
+            <div class="sixteen wide column">
+                <p><img src="images/LogoTZ_var2.jpg" alt="Logo Týden zdraví" width="148" height="200" /></p>
+                <p><a href="files/LOGO_Týden_zdraví.ai" target="_blank">Logo ve formátu AI</a></p>
+                <p><a href="files/LOGO_Týden_zdraví_1.png" target="_blank">Logo ve formátu PNG</a></p>
+                <p><a href="files/LOGO_Týden_zdraví_JPG_2.jpg" target="_blank">Logo ve formátu JPG</a></p>
+            </div>
+    </div>
+</div>
+
