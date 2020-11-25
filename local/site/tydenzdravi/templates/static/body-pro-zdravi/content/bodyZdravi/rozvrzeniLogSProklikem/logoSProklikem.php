1bb37e7404afd8af1942079e9ad8eeff37fd8ab7
Petr Svoboda <svoboda@grafia.cz>
2020-11-25 10:04:22 +0100

    oprava loca logoSProklikem

---
+++ local/site/tydenzdravi/templates/static/body-pro-zdravi/content/bodyZdravi/rozvrzeniLogSProklikem/logoSProklikem.php
@@ -0,0 +1,6 @@
+
+            <div class="column middle aligned">
+                <a <?= $this->attributes($odkazBodyAttributes) ?>>
+                    <img <?= $this->attributes($imagesBodyAttributes) ?> />
+                </a>
+            </div>
