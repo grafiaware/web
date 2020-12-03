176bc81cce73b5642f4ea06a8a3e6929d0fa166b
Petr Svoboda <svoboda@grafia.cz>
2020-12-03 17:11:08 +0100

    links

--- local/site/tydenzdravi/templates/layout/head/links.php
+++ local/site/tydenzdravi/templates/layout/head/links.php
@@ -1,11 +1,6 @@
 
-    <link rel="preload" as="font" crossorigin type="font/woff2" href="<?= $linksCommon.'semantic-ui/themes/default/assets/fonts/icons.woff2'?>">
-    <!--<link rel="preload" as="style" crossorigin type="text/css" href="<?= $linksSite.'semantic-ui/semantic.min.css'; ?>">-->
-    <link rel="preload" as="script" crossorigin type="text/javascript" href="<?= $linksSite.'semantic-ui/semantic.min.js'?>">
-
-        <link rel="stylesheet" type="text/css" href="<?= $linksSite.'semantic-ui/semantic.min.css'?>">
-
     <link rel="icon" href="<?= $linksSite.'favicon/TydenZdravi.ico'?>" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="<?= $linksSite.'semantic-ui/semantic.min.css'?>">
     <link rel="stylesheet" type="text/css" href="<?= $linksCommon.'css/layout.css'?>" />
     <link rel="stylesheet" type="text/css" href="<?= $linksCommon ?>css/templates.css">
     <link rel="stylesheet" type="text/css" href="<?= $linksCommon.'css/media.css'?>" />
