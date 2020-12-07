b5a3ab6f2126c469e37ee56f2ef2ac02c88c3a82
Petr Svoboda <svoboda@grafia.cz>
2020-12-07 12:01:21 +0100

    sync template branch - template vrsion local public, img

--- local/site/grafia/templates/layout/head/linkCss.php
+++
@@ -1,7 +0,0 @@
-    <link rel="icon" href="<?= $webSitePublicDir.'favicon/Grafia.ico'?>" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="<?= $webSitePublicDir.'semantic/dist/semantic.min.css'?>">
-    <link rel="stylesheet" type="text/css" href="<?= $webPublicDir.'styles/layout.css'?>" />
-    <link rel="stylesheet" type="text/css" href="<?= $webPublicDir.'styles/media.css'?>" />
-    <link rel="stylesheet" type="text/css" href="<?= $linksCommon ?>css/templates.css">
-    <link rel="stylesheet" type="text/css" href="<?= $webPublicDir.'styles/old/styles.css'?>" />
-    <?= $linkEditCss ?? '' ?>
--- local/site/grafia/templates/layout/head/scripts.php
+++ local/site/grafia/templates/layout/head/scripts.php
@@ -1,3 +1,4 @@
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous" ></script>
-    <script src="<?= $linksCommon.'js/menu.js'?>" ></script>
-    <script src="<?= $linksSite.'semantic-ui/semantic.min.js'?>" ></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous" ></script>
+    <?= $linkEditorJs ?? '' ?>
+    <script type="text/javascript" src="<?= $linksCommon.'js/menu.js'?>" ></script>
+    <script type="text/javascript" src="<?= $linksSite.'semantic-ui/semantic.min.js'?>" ></script>
