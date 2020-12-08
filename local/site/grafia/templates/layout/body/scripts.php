f08dc5e63bf591c6b85b9b0f18183098c04d8f6b
Kateřina Šlehoferová <slehoferova@grafia.cz>
2020-12-08 14:11:46 +0100

    Styly pro grafia

--- local/site/grafia/templates/layout/body/scripts.php
+++ local/site/grafia/templates/layout/body/scripts.php
@@ -7,4 +7,17 @@
     $('.ui.selection.dropdown')
       .dropdown()
     ;
+    
+    //Vyuziti lokalniho uloziste pro menu
+    //ulozeni pozice vertikalni rolovaci listy u menu v editacnim rezimu
+    $(".vertical.menu.edit li").click(function(){
+        var itemPosition = $(".svisle-menu").scrollTop();
+        localStorage.setItem('itemPosition', itemPosition);
+    });
+    $(document).ready(function(){
+        //po nacteni stranky menu odroluje na pozici, ktera se ulozila
+        $('.svisle-menu').scrollTop(
+            localStorage.getItem('itemPosition')
+        );
+    });
 </script>
