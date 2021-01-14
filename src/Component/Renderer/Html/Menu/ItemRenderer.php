36ec3a36af92e98a46dd9dafcf0ab59c73ea61c8
Petr Svoboda <svoboda@grafia.cz>
2021-01-14 14:33:50 +0100

    item rnderer

--- src/Component/Renderer/Html/Menu/ItemRenderer.php
+++ src/Component/Renderer/Html/Menu/ItemRenderer.php
@@ -34,7 +34,7 @@ class ItemRenderer extends HtmlRendererAbstract {
         $html = Html::tag('li',
                 ['class'=>[
                     $this->classMap->resolveClass($itemViewModel->isOnPath(), 'Item', 'li.onpath', 'li'),
-                    $this->classMap->resolveClass($itemViewModel->isLeaf(), 'Item', 'li.leaf', ($itemViewModel->getRealDepth() > 1) ? 'li.item' : 'li.dropdown'),
+                    $this->classMap->resolveClass($itemViewModel->isLeaf(), 'Item', 'li.leaf', ($itemViewModel->getRealDepth() == 1) ? 'li.dropdown' : 'li.item'),
                     $this->classMap->resolveClass($itemViewModel->isPresented(), 'Item', 'li.presented', 'li'),
                     ],
                  'data-red-onpath'=>$itemViewModel->isOnPath() ? "1" : "0",
