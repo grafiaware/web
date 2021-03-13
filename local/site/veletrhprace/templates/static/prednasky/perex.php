7f010813c02ddd5205906c594179348a20106770
Petr Svoboda <svoboda@grafia.cz>
2021-03-13 15:43:56 +0100

    merge 12.3. + static prednasky poradny

---
+++ local/site/veletrhprace/templates/static/prednasky/perex.php
@@ -0,0 +1,19 @@
+<?php
+use Pes\View\Renderer\PhpTemplateRendererInterface;
+use Model\Entity\PaperAggregatePaperContentInterface;
+use Pes\Text\Text;
+use Pes\Text\Html;
+
+/** @var PhpTemplateRendererInterface $this */
+/** @var PaperAggregatePaperContentInterface $paperAggregate */
+
+    $text =
+    '
+Další přednášky průběžně doplňujeme, koukněte sem za pár dnů!
+
+Přednášky můžete i opakovaně shlédnout na našem youtube kanálu. Odkaz a konkrétní čas najdete zde po 20. 3. 2021
+';
+
+    echo Html::p(Text::mono($text), ["class"=>"velky text"])
+?>
+
