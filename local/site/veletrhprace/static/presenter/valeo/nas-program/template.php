5274d3710af990cb536657e327d28f56a21ca9b9
Kateřina Šlehoferová <slehoferova@grafia.cz>
2021-03-29 18:09:20 +0200

    program / pohovor ve firmách, styly

--- local/site/veletrhprace/static/presenter/valeo/nas-program/template.php
+++ local/site/veletrhprace/static/presenter/valeo/nas-program/template.php
@@ -1,21 +1,31 @@
 <?php
+use Site\Configuration;
+use Model\Arraymodel\EventList;
+
 use Pes\View\Renderer\PhpTemplateRendererInterface;
 use Model\Entity\PaperAggregatePaperContentInterface;
+use Model\Repository\StatusSecurityRepo;
 
+    $statusSecurityRepo = $container->get(StatusSecurityRepo::class);
+    /** @var StatusSecurityRepo $statusSecurityRepo */
+    $statusSecurity = $statusSecurityRepo->get();
+    $eventTypeName = "Prezentace, Přednáška";  // viz Model\Arraymodel\EventType
+    $institutionName = "Valeo Autoklimatizace";
+    $event = (new EventList($statusSecurity))->getEventList($eventTypeName, $institutionName, [], true);   // enrolling = true
+
+    
+    
+    $headline = 'Náš program';
+    $perex =
+        '
+        ';
+    $footer = '';
 ?>
 
-<article class="paper">
-    <section>
-        <headline>
-            <?php // include "headline.php" ?>
-        </headline>
-        <perex>
-            <?php // include "perex.php" ?>
-        </perex>
-    </section>
-    <section>    
-        <content>
-            <?php // include "content/stanek.php" ?>
-        </content>
-    </section>
-</article>
+ 
+    <div id="nas-program">
+        <?php
+        //include Configuration::componentControler()['templates']."timecolumn/template.php";
+        include Configuration::componentControler()['templates']."timeline-boxes/template.php";
+        ?>
+    </div>
