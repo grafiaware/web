cf1afb3629e88e0d2adc207e10a77a3627183b16
Kateřina Šlehoferová <slehoferova@grafia.cz>
2021-03-30 12:10:25 +0200

    Templates presenter-contacts

--- local/site/veletrhprace/templates/presenter-contacts/content/kontakty.php
+++ local/site/veletrhprace/templates/presenter-contacts/content/kontakty.php
@@ -10,17 +10,17 @@ use Pes\Text\Html;
 ?>
 <div class="navazat-kontakt">
     <div class="ui grid">
-        <div class="sixteen wide column center aligned">
-            <?= $this->insert(__DIR__.'/kontakty/profilove-udaje.php') ?>
+<!--        <div class="sixteen wide column center aligned">
+            <?='';// $this->insert(__DIR__.'/kontakty/profilove-udaje.php') ?>
         </div>
         <div class="sixteen wide column center aligned">
             <div class="ui large button profil-visible">Přeji si vyplnit své osobní údaje</div>
         </div>
         <div class="sixteen wide column">
             <div class="profil hidden">
-                <?= $this->insert(__DIR__.'/kontakty/osobni-udaje.php') ?>
+                <?= '';//$this->insert(__DIR__.'/kontakty/osobni-udaje.php') ?>
             </div>
-        </div>
+        </div>-->
         <div class="sixteen wide column">
             <content>
                 <div class="kontaktni-udaje-firmy">
