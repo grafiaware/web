20854071b3bd472a03eb5a2766a94e552480e0de
Kateřina Šlehoferová <slehoferova@grafia.cz>
2021-04-01 18:18:18 +0200

    Stránka Pro média

---
+++ local/site/veletrhprace/static/pro-media/content/tiskova-zprava/fotky.php
@@ -0,0 +1,10 @@
+<?php
+use Pes\View\Renderer\PhpTemplateRendererInterface;
+use Model\Entity\PaperAggregatePaperContentInterface;
+use Pes\Text\Text;
+use Pes\Text\Html;
+/** @var PhpTemplateRendererInterface $this */
+/** @var PaperAggregatePaperContentInterface $paperAggregate */
+
+?>
+
--- local/site/veletrhprace/static/pro-media/data.php
+++ local/site/veletrhprace/static/pro-media/data.php
@@ -11,6 +11,52 @@ $perex = 'Budeme rádi za zveřejnění tiskových zpráv k Veletrhu práce a vz
 
 $tiskovaZprava = [
     [
+        'idZpravy' => '<b>Tisková zpráva č. 3</b><br/> 1. 4. 2021',
+        'nazev' => 'Veletrh práce a vzdělávání Klíč k příležitostem ukázal výzvy i bariéry v online komunikaci',
+        'text' =>
+        '
+Třídenní <b>Veletrh práce a vzdělávání</b> nabídl nejen pracovní místa na virtuálních stáncích firem, ale také <b>pracovní i oddechová témata v hlavním přednáškovém programu. Celá akce včetně individuálních online poraden byla pro návštěvníky zdarma. </b>
+
+Hlavní program byl vysílán na <a href="https://www.youtube.com/channel/UC-Di-88rpUfBZUHHVf7tntQ/videos" target="_blank">youtube kanálu veletrhu</a>, kde je uložen i pro pozdější shlédnutí po dobu dalších 3 měsíců.
+Naopak <a href="www/item/cs/604bd0e2e440d" target="_blank">poradenský program</a> a online setkání s personalisty na <a href="www/item/cs/60619d3247985" target="_blank">virtuálních stáncích</a> byly k dispozici jen po dobu veletrhu.
+
+Mezi vystavujícími byly významné podniky, např. <b>AKKA Czech Republic, Drůbežářský závod Klatovy, Wienerberger, Daikin, Kermi, Possehl Electronics Czech Republic, Konplan, STOELZLE UNION či Valeo Autoklimatizace.</b>
+Každá firma prezentovala v hlavním programu na youtube, který lze shlédnout ze záznamu.
+
+Mezi nabízenými pozicemi <b>převládaly technické a  výrobní profese</b>. Spektrum bylo opravdu široké, od dělnických přes technické až po manažerské: testovací inženýr, strojní technik, skladník technického materiálu, datový analytik, technik kamerových systémů, konstruktér, pracovník technické dokumentace, míchač, obsluha udíren, obsluha balících a etiketovacích strojů, strojník, seřizovač kontrolních automatů, dělník keramické výroby, obsluha lisu, provozní elektrikář, řidič VZV, řidič, kolového nakladače, operátor call centra, třídička, vedoucí projektů ve výrobě, plánovač / normovač, pracovník zákaznických objednávek, servisní technik, referent nákupu, manažer technického servisu - Vstřikování plastů, lisař, logistik, operátor/ka výroby, kontrolor/ka kvality a další.
+
+Virtuální veletrh na webu <a href="https://veletrhprace.online/" target="_blank">www.veletrhPRACE.online/</a> navštívilo přes 2000 návštěvníků, kteří v průměru viděli 14 stránek, jak uvádějí statistiky webu. Asi polovina návštěvníků se opakovaně vracela. Návštěvníci se mohli připojit z počítače, z mobilu i tabletu, dle zpětné vazby web vnímali jako uživatelsky přátelský. Jen do online místností se návštěvníkům moc nechtělo, přestože byla možnost vstoupit i bez kamery či pouze chatovat.
+
+"Máme radost z návštěvnosti," říká Jana Brabcová, jednatelka pořádající společnosti Grafia. "Oslovili jsme některé návštěvníky s dotazem, proč nevstoupili do virtuálních místností. Řada z nich uvedla, že si nejsou jisti svými digitálními dovednostmi či stabilitou svého připojení, jiní, že měli strach, kdo bude na druhé straně obrazovky a jestli je někdo nebude tajně nahrávat." 
+Pokud byl u poraden uveden i telefon, někteří návštěvníci raději zavolali. "Zdá se, že přestože už rok většina domácností funguje v online světě díky svým dětem, pokud jde o online kontakt s cizím člověkem, kterého jsme nikdy předtím neviděli, jsme opatrní. Myslím, že pokud i nadále ve firemním prostředí zůstane online jako paralelní realita, musíme se ještě hodně učit - o bezpečnosti na internetu i psychologii člověka. Určitě je to velká výzva," dodává Brabcová, která je zároveň prezidentkou Asociace institucí vzdělávání dospělých. 
+ 
+Jan Neckář z Diakonie Západ, která poskytovala poradenství v občanské poradně, v poradně pro cizince i psychologické poradenství, konstatoval, že odvážní byli pouze jednotlivci, kteří přišli konzultovat složité situace se zaměstnavatelem, hledání práce i rodinné problémy. Nikdo z návštěvníků ale neměl zapnutou kameru. "Připojili se i někteří, kteří na oslovení nereagovali a záhy virtuální místnost opustili, patrně se spíš lekli, než že by neměli technické dovednosti," říká Neckář.
+Podobnou zkušenost měli i kariéroví poradci ze společnosti Grafia a EURES poradkyně z plzeňské krajské pobočky úřadu práce, Markéta Vondrová. "Jednotliví klienti se připojili bez kamery, jeden dokonce z Německa. Bariérou je často špatné připojení a nezkušenost v práci s novodobými aplikacemi v online komunikaci. Dotazy směřovaly k hledání zaměstnání v Německu, vycestování a k aktuálním podmínkám a situaci na trhu práce."
+
+Naopak pracovní psycholožka Elena Wagnerová uvedla, že pozorovala odlišné chování mladších a starších klientů. Zatímco mladší nechtěli použít kameru, padesátníci neměli zábrany ukázat svou identitu. Ti přišli konzultovat opravdu složité pracovní i osobní problémy. "Nemohu sdělovat podrobnosti, ale s některými jsem konzultovala i dvě hodiny a další den se přihlásili znovu. Možná jim zapnuté kamery naopak pomohly s důvěrou a byli velmi otevření. Ve spolupráci s kariérovými poradci se podařilo navrhnout konkrétní postup a možnosti návratu na trh práce."
+
+O své dojmy se podělil i návštěvník Jakub B., který se zajímal o místo programátora: "Web veletrhu byl pro mne srozumitelný, registrace vyžadovala jen jméno a emailovou adresu, což mne potěšilo. Díky tomu jsem mohl na individuální pohovory k firmám, ale odhodlával jsem se dva dny. Měl jsem obavu, co mne čeká, pro jistotu jsem měl puštěný pouze zvuk. Personalistka byla velmi milá, nijak na mne netlačila, všechno trpělivě vysvětlila. Asi jim pošlu životopis, ale na pohovor bych pak raději osobně."
+
+Přes tisíc návštěvníků sledovalo přednášky na youtube kanálu veletrhu. Mezi nejúspěšnější témata patřilo Jak oslovit zaměstnavatele a připravit se na pracovní pohovor, Jaká je budoucnost absolventů VŠ, Pracovně právní problematika v době covidu, Zvolené rekvalifikace zdarma, ale také některé firemní prezentace.
+
+Vendula Linková, HR manažerka Konplan, sdělila v interview na téma home office, že se její společnost ráda bude vracet k živé komunikaci. "Online je velká velká příležitost, ale na řadu věcí je třeba vidět se osobně, realizujeme běžně online pohovory. Je ale jasné, že osobní kontakt řekne víc. Někteří naši manažeři dokonce preferují pouze offline kontakt s uchazečem o práci, zejména v IT." I její spolupracovníci na virtuálním stánku potvrzují zkušenost s nízkým počtem odvážlivců, kteří vstupovali do online místnosti téměř vždy s vypnutými kamerami, ať už na pohovor či přednášku. 
+
+Na veletrhu jako každoročně byly také vyhlášeny ceny pro podniky přátelské rodině Mamma/Parent Friendly za rok 2020. Letos je získaly tyto společnosti: BOHEMIA SEKT, RSF Elektronik a Sony DADC Czech Republic.
+
+<b>Na pozice nabízené na veletrhu se návštěvníci mohou hlásit i po ukončení akce celý duben přes svůj <a href="www/item/cs/6062d0e00190e" target="_blank">návštěvnický profil</a>, kam vloží svůj životopis a motivační dopis. Po kliknutí na pracovní pozici u firemního stánku lze zanechat dokumenty personalistům. </b>
+
+<div class="ui three column grid stackable">
+<div class="column"><a href="images/printscreen1.jpg" download="Foto z veletrhu"><img src="images/printscreen1.jpg" alt="Foto z akce" height="" width="100%"/>Stáhnout</a><p class="text maly okraje-vertical">Firemní prezentace Konplan</p></div>
+<div class="column"><a href="images/printscreen1.jpg" download="Foto z veletrhu"><img src="images/printscreen2.jpg" alt="Foto z akce" height="" width="100%"/>Stáhnout</a><p class="text maly okraje-vertical">Jak přemýšlí personalista náborář - interview se zástupci ČEZ a ASSA ABLOY</p></div>
+<div class="column"><a href="images/printscreen1.jpg" download="Foto z veletrhu"><img src="images/printscreen3.jpg" alt="Foto z akce" height="" width="100%"/>Stáhnout</a><p class="text maly okraje-vertical">Firmy BOHEMIA SEKT, RSF Elektronik a Sony DADC Czech Republic získaly Mamma / Parent Friendly</p></div>
+</div>
+
+
+  
+        '
+
+    ],
+    [
         'idZpravy' => '<b>Tisková zpráva č. 2</b><br/> 20. 3. 2021',
         'nazev' => 'Pracovní místa, přednášky i poradny, to je online Veletrh práce a vzdělávání',
         'text' =>
