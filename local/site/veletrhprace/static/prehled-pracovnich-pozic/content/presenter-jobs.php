9cea5300d1d45082e0ce1f8a201c77c979b115a0
Petr Svoboda <svoboda@grafia.cz>
2021-04-15 00:44:22 +0200

    odkaz na stánek do výpidu pozi - chci odeslat údaje

--- local/site/veletrhprace/static/prehled-pracovnich-pozic/content/presenter-jobs.php
+++ local/site/veletrhprace/static/prehled-pracovnich-pozic/content/presenter-jobs.php
@@ -8,5 +8,19 @@ use Site\Configuration;
 ?>
             <div id="<?= $shortName ?>">
                 <p class="velky text primarni-barva tucne"><?= $presenterName ?></p>
+
+                <?php
+                if (isset($block)) {
+                    ?>
+                    <a href="<?= "www/block/".$block->getName()."#chci-navazat-kontakt" ?>">
+                        <div class="ui button grey">
+                            Chci jít na stánek pro kontaktní údaje
+                        </div>
+                    </a>
+                    <?php
+                }
+                ?>
+
+
                 <?= $this->insert( Configuration::componentControler()['templates']."presenter-job/content/vypis-pozic.php", $presenterJobs); ?>
             </div>
