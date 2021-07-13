73612cdc898d162d56691da2bc92fb3eaac88a45
Kateřina Šlehoferová <slehoferova@grafia.cz>
2021-07-13 14:26:18 +0200

    Templates article

---
+++ local/site/common/templates/article/product_page/template.php
@@ -0,0 +1,52 @@
+<article class="edit-html borderDance">
+    <div class="ui stackable centered grid">
+        <p class="nadpis podtrzeny nastred nadpis-scroll show-on-scroll edit-text borderDance">Název produktu</p>
+        <div class="sixteen wide column">
+            <p class="text velky primarni-barva">Popis:</p>
+            <div class="float-img vpravo">
+                floatovací obrázek
+            </div>
+            <p>Potřebujete sdělovat víc, než jen co vyrábíte? Zákazníci mají plné koše prospektů a přesto kupují u konkurence? Dobrá znalost je základem důvěry. Dejte zákazníkům šanci, aby Vás poznali!</p>
+            <p>Vyrobíme Váš firemní časopis tak, aby jej Vaši zákazníci rádi četli a Vy jste měli minimum práce s jeho přípravou. K dispozici Vám dáme naše zkušené redaktory, fotografy, korektory, sazeče a grafiky. Navrhneme layout časopisu, zajistíme tisk, rozeslání na Vaše zákazníky a zajistíme zákonné povinnosti za Vás.</p>
+            <p>Lze vybírat z mnoha formátů a typů časopisů. Nejčastěji užívaným formátem je A4, A5, ale lze využít libovolně velký formát. Tisk může být plnobarevný nebo třeba jen jednobarevný, můžete vydávat třeba vlastní noviny...</p>
+        </div>
+        <div class="sixteen wide column">
+            <div class="ui two column grid internally celled stackable centered">
+                <div class="seven wide column edit-html borderDance">
+                    <p class="text velky primarni-barva">Výhody časopisu:</p>
+                    <ul>
+                        <li>Časopis zdarma přináší novinky z oboru až na stůl Vašeho zákazníka</li>
+                        <li>Mnohem častěji sáhne zákazník po časopisu než po prospektu</li>
+                        <li>Časopis zlepší Vaše image</li>
+                        <li>Časopis podporuje loajalitu Vašich zákazníků</li>
+                    </ul>
+                </div>
+                <div class="seven wide column edit-html borderDance">
+                    <p class="text velky primarni-barva">Proč spolupracovat s námi:</p>
+                    <ul>
+                        <li>Nápady, které fungují</li>
+                        <li>Servis do nejmenšího detailu</li>
+                        <li>Komunikace s jedním dodavatelem, který Vás dobře zná a dobře Vám poradí</li>
+                        <li>Sami určujete design a obsah informací</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="sixteen wide column">
+            <div class="ui three column grid stackable">
+                <div class="column">obrázek 1</div>
+                <div class="column">obrázek 2</div>
+                <div class="column">obrázek 3</div>
+                <div class="column">obrázek 4</div>
+                <div class="column">obrázek 5</div>
+            </div>
+        </div>
+        <div class="eight wide column centered">
+            <p class="text okraje-vertical"></p>
+            <div class="ui piled segment">
+                <p class="text velky primarni-barva">Kontakujte nás:</p>
+                <p class="text zadne-okraje"><b>Telefon:</b> 378 771 226; 774 484 856</p>
+                <p class="text zadne-okraje"><b>E-mail:</b> produkce@grafia.cz</p>
+            </div>
+        </div>
+</article>
---
+++ local/site/common/templates/article/retraining_course/template.php
@@ -0,0 +1,55 @@
+<article class="">
+    <div class="ui stackable centered grid">
+        <div class="sixteen wide column">
+            <div class="dva-sloupce-nadpis">
+                <p class="nadpis podtrzeny nastred nadpis-scroll show-on-scroll edit-text borderDance">Název kurzu</p>
+                <div class="ui two column stackable centered mobile reversed grid">
+                    <div class="six wide column middle aligned edit-html borderDance">
+                        <p>Obrázek</p>
+                    </div>
+                    <div class="ten wide column edit-html borderDance">
+                        <p><b>Levý sloupec se hodí spíše na obrázek, protože je zarovnaný vertikálně na střed tohoto sloupce. Napište úvod ke kurzu, text může být libovolně dlouhý.</b></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sixteen wide column">
+            <div class="edit-html borderDance">
+                <p class="velky text primarni-barva nastred okraje-vertical">Obsah kurzu</p>
+                <ul>
+                    <li>Orientace v základní terminologii andragogiky, didaktiky, hodnocení a měření ve vzdělávání dospělých a lektorských forem a metod ve vzdělávání dospělých</li>
+                    <li>Sestavení obsahu a struktury vzdělávacího programu na základě stanovených vzdělávacích cílů, vč. z nich vyplývajících kompetencí</li>
+                    <li>Rozpracování obsahu kurzu do minutového scénáře včetně volby didaktických pomůcek a metod výuky</li>
+                    <li>Zjišťování úrovně vstupních kompetencí a vzdělávacích potřeb účastníků vzdělávacího programu</li>
+                </ul>
+            </div>
+        </div>
+        <div class="sixteen wide column">
+            <div class="edit-html borderDance">
+                <p class="velky text primarni-barva nastred okraje-vertical">Doklad</p>
+                <p>Při splnění minimálně 80% docházky do kurzu absolventi a absolventky získají Potvrzení o účasti v akreditovaném vzdělávacím programu. Absolventi a absolventky, kteří na konci kurzu úspěšně složí zkoušku z profesní kvalifikace, získají Osvědčení o získání profesní kvalifikace "Lektor dalšího vzdělávání" dle zákona č. 179/2006 Sb. <a href="">Více zde</a></p>
+            </div>
+        </div>
+        <div class="sixteen wide column">
+            <p class="text okraje-vertical"></p>
+            <div class="ui segment edit-html borderDance">
+                <p class="velky text primarni-barva nastred">Základní informace o kurzu</p>  
+                <p class="text zadne-okraje"><i class="calendar alternate outline icon"></i> <b>Termín kurzu:</b> 31. 5. - 16. 7. 2021</p>
+                <p class="text zadne-okraje"><i class="chart pie icon"></i> <b>Rozsah kurzu:</b> 100 hodin výuky + zkouška</p>
+                <p class="text zadne-okraje"><i class="users icon"></i> <b>Počet účastníků:</b> Cca 5 - 12 osob</p>
+                <p class="text zadne-okraje"><i class="address book outline icon"></i> <b>Vstupní požadavky:</b> Střední vzdělání s maturitní zkouškou</p>
+                <p class="text zadne-okraje"><i class="map marked alternate icon"></i> <b>Místo konání:</b> Plzeň</p>
+                <p class="text zadne-okraje"><i class="money bill icon"></i> <b>Cena:</b> 0000 Kč</p>
+                <p class="text nastred"><a class="ui labeled icon primary large button" href=""><i class="pencil alternate icon"></i> Vyplnit přihlášku</a></p>
+            </div>
+        </div>
+        <div class="sixteen wide column">
+            <div class="edit-html borderDance">
+                <p class="velky text primarni-barva nastred okraje-vertical">Řekli o kurzu</p>
+                <p class="text kurziva zadne-okraje">"Velmi zdařilé spojení teorie a příkladů z předchozí pestré praxe na různých pozicích."</p>
+                <p class="text kurziva zadne-okraje">"Jen tak dál, je radost být účastníkem Vašeho kurzu."</p>
+                <p class="text kurziva zadne-okraje">"Lektorka Petra na mne působila jako výborný lektor. Má velmi pěkný přednes. Usměvavá, ve všem nápomocná. Umí poradit, zaujmout. Výborná lektorka."</p>
+            </div>
+        </div>
+    </div>
+</article>
