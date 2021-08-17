57d6dd233979a2c91d0f6977c3f575ac1aa8cfa8
Petr Svoboda <svoboda@grafia.cz>
2021-08-17 15:15:22 +0200

    malá písmena

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 define('PROJECT_PATH', str_replace("\\", "/", preg_replace('/^' . preg_quote($_SERVER['DOCUMENT_ROOT'], '/') . '/', '', __DIR__))."/");
 
-include 'vendor/pes/pes/src/Bootstrap/Bootstrap.php';
+include 'vendor/Pes/pes/src/Bootstrap/Bootstrap.php';
 //echo PES_RUNNING_ON_PRODUCTION_HOST ? "<p>Production host</p>" : "<p>No production host</p>";
 //echo "<pre>".print_r($bootstrapLoggerArray, true)."</pre>";
 
