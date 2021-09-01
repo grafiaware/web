ed6d4bbe734555b49ff0f049238e2969bcdbcbc5
Petr Svoboda <svoboda@grafia.cz>
2021-09-01 14:23:38 +0200

    přeskládání front kontrolerů, odstraněn StatusFronControlerAbstract

--- src/FrontControler/FrontControlerAbstract.php
+++ src/FrontControler/FrontControlerAbstract.php
@@ -11,6 +11,8 @@ namespace FrontControler;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Container\ContainerInterface;
+use Pes\Application\AppFactory;
+use Pes\Application\UriInfoInterface;
 
 /**
  * Description of ControllerAbstract
@@ -41,5 +43,34 @@ abstract class FrontControlerAbstract implements FrontControlerInterface {
     public function injectContainer(ContainerInterface $componentContainer): FrontControlerInterface {
         $this->container = $componentContainer;
         return $this;
+    }
+
+    ### uri info helpers ###
+
+    /**
+     * Vrací base path pro nastavení html base path
+     * @param ServerRequestInterface $request
+     * @return string
+     */
+    protected function getBasePath(ServerRequestInterface $request) {
+        return $this->getUriInfo($request)->getSubdomainPath();
+    }
+
+    /**
+     * Vrací relativní path pro redirect url
+     * @param ServerRequestInterface $request
+     * @return type
+     */
+    protected function getRedirectPath(ServerRequestInterface $request) {
+        return $this->getUriInfo($request)->getSubdomainPath();
+    }
+
+    /**
+     * Pomocná metoda - získá base path z objektu UriInfo, který byl vložen do requestu jako atribut s jménem AppFactory::URI_INFO_ATTRIBUTE_NAME v AppFactory.
+     *
+     * @return UriInfoInterface
+     */
+    private function getUriInfo(ServerRequestInterface $request) {
+        return $request->getAttribute(AppFactory::URI_INFO_ATTRIBUTE_NAME);
     }
 }
