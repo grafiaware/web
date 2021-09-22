e3c845e11f103dde8dc7aad5a836cfaaf85387f0
Petr Svoboda <svoboda@grafia.cz>
2021-09-22 08:33:57 +0200

    select template classy v rendererech a tinyInit - fix

--- public/site/common/js/affixScript.js
+++ public/site/common/js/affixScript.js
@@ -5,12 +5,18 @@
 function loadError(oError) {
   throw new URIError("The script " + oError.target.src + " didn't load correctly.");
 }
-
+function onLoad() {
+    alert("The script \"myScript2.js\" has been correctly loaded.");
+}
 function affixScriptToHead(url, onloadFunction) {
   var newScript = document.createElement("script");
   newScript.onerror = loadError;
   if (onloadFunction) { newScript.onload = onloadFunction; }
-  document.head.appendChild(newScript);
+  document.head.appendChild(newScript);  // do head
+  //document.currentScript.parentNode.insertBefore(newScript, document.currentScript);  //před aktuální sktipt (třeba do body
   newScript.src = url;
 }
 
+// affixScriptToHead("myScript1.js");
+// affixScriptToHead("myScript2.js", function () { alert("The script \"myScript2.js\" has been correctly loaded."); });
+// affixScriptToHead("myScript1.js", ??);
