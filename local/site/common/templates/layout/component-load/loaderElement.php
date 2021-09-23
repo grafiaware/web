1dec0fbb71702eaeefa27744853a90c487f041fc
Petr Svoboda <svoboda@grafia.cz>
2021-09-23 11:09:36 +0200

    loaderElement upg

--- local/site/common/templates/layout/component-load/loaderElement.php
+++ local/site/common/templates/layout/component-load/loaderElement.php
@@ -3,6 +3,6 @@ $onloadScript = "replaceElement('red_loaded_$loaderWrapperElementId', '$apiUri')
 ?>
 <div id="red_loaded_<?=$loaderWrapperElementId?>" class="red_loaded">
     <script>
-        replaceElement("red_loaded_<?=$loaderWrapperElementId?>", "<?= $apiUri?>");
+        <?= "replaceElement('red_loaded_$loaderWrapperElementId', '$apiUri');" ?>
     </script>
 </div>
