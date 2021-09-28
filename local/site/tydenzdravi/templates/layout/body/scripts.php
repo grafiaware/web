0f5736502038003a95c908a3d8bf0b8606dd48d3
Petr Svoboda <svoboda@grafia.cz>
2021-09-28 22:19:10 +0200

    kakádní načtení obsahu pomocí Promise - body.js, cascade.js

--- local/site/tydenzdravi/templates/layout/body/scripts.php
+++ local/site/tydenzdravi/templates/layout/body/scripts.php
@@ -1,3 +1,3 @@
-<?= $linkEditorJs ?? '' ?>
+    <script type="text/javascript" src="<?= $linksCommon.'js/cascade.js'?>" ></script>
+    <script type="text/javascript" src="<?= $linksCommon.'js/body.js'?>" ></script>
 
-<script type="text/javascript" src="<?= $linksCommon.'js/body.js'?>" ></script>
--- local/site/veletrhprace/templates/layout/body/scripts.php
+++ local/site/veletrhprace/templates/layout/body/scripts.php
@@ -1,8 +1,3 @@
-<?= $linkEditorJs ?? '' ?>
-
-<!-- scripty pro socialni site-->
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v10.0" nonce="YwCvYlBF"></script> 
-<script async src="//www.instagram.com/embed.js"></script>
-
-<script type="text/javascript" src="<?= $linksCommon.'js/body.js'?>" ></script>
+    <script type="text/javascript" src="<?= $linksCommon.'js/cascade.js'?>" ></script>
+    <script type="text/javascript" src="<?= $linksCommon.'js/body.js'?>" ></script>
 
