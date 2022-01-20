3529a1bd48d5e7fe436c9eeea5d11a4d1687794b
Kateřina Šlehoferová <slehoferova@grafia.cz>
2022-01-20 14:44:21 +0100

    Lepík jinak znovu

--- src/Component/Renderer/Html/Authored/Article/ArticleRendererEditable.php
+++ src/Component/Renderer/Html/Authored/Article/ArticleRendererEditable.php
@@ -2,8 +2,6 @@
 namespace Component\Renderer\Html\Authored\Article;
 
 use Red\Model\Entity\ArticleInterface;
-use Component\View\Authored\Article\ArticleComponent;
-use Component\View\Authored\AuthoredComponentAbstract;
 
 use Component\ViewModel\Authored\Article\ArticleViewModelInterface;
 use Pes\Text\Html;
@@ -19,46 +17,46 @@ class ArticleRendererEditable extends HtmlRendererAbstract {
     public function render(iterable $viewModel=NULL) {
         /** @var ArticleViewModelInterface $viewModel */
         $article = $viewModel->getArticle();  // vrací ArticleInterface
-        $active = $viewModel->getMenuItem()->getActive();
-
+        $active = $viewModel->isMenuItemActive();
+        $buttonEditContent = (string) $viewModel->getContextVariable('buttonEditContent') ?? '';
+        $selectTemplate = (string) $viewModel->getContextVariable('selectTemplate') ?? '';
+        $articleButtonsForm = $this->renderArticleButtonsForm($article);
 
         if (isset($article)) { // menu item aktivní (publikovaný)
             $templateName = $article->getTemplate() ?? '';
             /** @var ArticleInterface $article */
             if ($templateName) {
                 $formContent = [
-                                Html::tag('div', ['class'=>$this->classMap->get('Content', 'div.templateArticle')],
-                                    Html::tag('article',
+                                
+                                Html::tag('div',
                                         [
                                             'id'=>'article_'.$article->getId(),
                                             'class'=>'edit-html',
                                             "data-templatename"=>$templateName,   // toto je selektor pro template css - nastaveno v base-template.less souboru
                                         ],
                                         $article->getContent()),  // co je editovatelné je dáno šablonou
-                                )
                             ];
             } else {
                 $formContent = [
                                 Html::tag('input', ['type'=>'hidden', 'name'=>'article_'.$article->getId()]),  // hidden input pro Article Controler update
-                                Html::tag('div', ['class'=>$this->classMap->get('Content', 'div.templateArticle')],
-                                    Html::tag('article',
+                                    Html::tag('div',
                                         [
                                             'id'=>'article_'.$article->getId(),
                                             'class'=>'edit-html',
                                             "data-templatename"=>$templateName,   // toto je selektor pro template css - nastaveno v base-template.less souboru
                                         ],
                                         $article->getContent()),  // editovatelný celý obsah pokud nebyla použita šablona
-                                )
                             ];
             }
-            $ret = Html::tag('section', [
+            $ret = 
+                Html::tag('div', ['class'=>$this->classMap->get('Content', 'div.templateArticle')],    
+                    Html::tag('article', [
                                         'data-red-renderer'=>'ArticleRendererEditable',
                                         "data-red-datasource"=> "article {$article->getId()} for item {$article->getMenuItemIdFk()}",
                                         ],
                         [
-                            $viewModel->getContextVariable(AuthoredComponentAbstract::BUTTON_EDIT_CONTENT) ?? '',
-                            $viewModel->getContextVariable(ArticleComponent::SELECT_TEMPLATE) ?? '',
-                            Html::tag('div', ['class'=>$this->classMap->get('PaperButtons', 'div.ribbon-disabled')]), //lepítko s buttony
+                            $buttonEditContent,
+                            Html::tag('div', ['class'=>$this->classMap->get('PaperButtons', 'div.ribbon-article')], //lepítko s buttony
                             Html::tag('div', ['class'=>$this->classMap->get('Content', 'div.semafor')], //aktivní/neaktivní paper
                                 Html::tag('div',
                                    [
@@ -71,13 +69,64 @@ class ArticleRendererEditable extends HtmlRendererAbstract {
                                        ]
                                     )
                                 )
+                                )
+                                .Html::tag('div', ['class'=>$this->classMap->get('Content', 'div.nameMenuItem')],
+                                    Html::tag('p', ['class'=>''],
+                                        'Typ: Article'
+                                    )
+                                    .Html::tag('p', ['class'=>''],
+                                        'Název položky menu'
+                                    )
+                                )
+                                .$articleButtonsForm
+                                    
                             ),
+                            $selectTemplate ?? '',
                             Html::tag('form', ['method'=>'POST', 'action'=>"red/v1/article/{$article->getId()}"],
                                 $formContent
                             )
                         ]
+                        )
                     );
         }
         return $ret ?? '';
     }
+    
+     private function renderArticleButtonsForm(ArticleInterface $article) {
+        $articleId = $article->getId();
+
+        $btnAktivni =  Html::tag('button', [
+                    'class'=>$this->classMap->get('PaperButtons', 'button'),
+                    'data-tooltip'=> 'Publikovat / Nepublikovat', //$active ? 'Nepublikovat' : 'Publikovat',
+                    'data-position'=>'top right',
+                    'tabindex'=>'0',
+                    'formtarget'=>'_self',
+                    'formmethod'=>'post',
+                    'formaction'=>"",
+                    ],
+                    Html::tag('i', ['class'=>$this->classMap->get('CommonButtons', 'button.notpublish')])
+                    //Html::tag('i', ['class'=>$this->classMapEditable->resolve($active, 'CommonButtons', 'button.notpublish', 'button.publish')])
+                );
+        $btnDoKose =   Html::tag('button', [
+                    'class'=>$this->classMap->get('PaperButtons', 'button'),
+                    'data-tooltip'=> 'Odstranit položku',
+                    'data-position'=>'top right',
+                    'tabindex'=>'0',
+                    'formtarget'=>'_self',
+                    'formmethod'=>'post',
+                    'formaction'=>"",
+                    'onclick'=>"return confirm('Jste si jisti?');"
+                    ],
+                    Html::tag('i', ['class'=>$this->classMap->get('CommonButtons', 'button.movetotrash')])
+                );
+        
+        
+        return Html::tag('form', ['method'=>'POST', 'action'=>""],
+            Html::tag('div', ['class'=>$this->classMap->get('PaperButtons', 'div.buttonsWrap')],
+                Html::tag('div', ['class'=>$this->classMap->get('PaperButtons', 'div.buttons')],
+                    $btnAktivni.$btnDoKose
+                )
+            )
+        );
+}
 }
