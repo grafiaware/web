cdd71b28668ab914e8d2951bf8c1438077215916
grafiaware <grafiaware@grafia.cz>
2022-02-08 15:28:16 +0100

    mionr fixes

--- src/Component/Renderer/Html/Manage/ToggleEditContentButtomRenderer.php
+++ src/Component/Renderer/Html/Manage/ToggleEditContentButtomRenderer.php
@@ -21,22 +21,31 @@ class ToggleEditContentButtomRenderer extends HtmlRendererAbstract {
             $tooltip = 'Vypnout editaci';
             $action = "red/v1/itemaction/{$viewModel->getItemType()}/{$viewModel->getItemId()}/remove";
         } else {
+            $action = $viewModel->getItemAction();
+            if (isset($action)) {
+                $editor = $action->getEditorLoginName() ?? '';
+                $disabled = 'disabled';
+            }
             $tooltip = 'Zapnout editaci';
             $action = "red/v1/itemaction/{$viewModel->getItemType()}/{$viewModel->getItemId()}/add";
         }
+
         return
             Html::tag('div', ['class'=>$this->classMap->get('PaperButtons', 'div.editMode')], //tlačítko "tužka" pro zvolení editace
                 Html::tag('form', ['method'=>'POST', 'action'=>$action],
-                    Html::tag('button', [
-                        'class'=>$this->classMap->resolve($userPerformActionWithContent, 'PaperButtons', 'div.offEditMode button', 'div.editMode button'),
-                        'data-tooltip' => $tooltip,
-                        'name' => UserActionControler::FORM_USER_ACTION_EDIT_CONTENT,
-                        'value' => '',
-                        'type' => 'submit',
-                        'formtarget' => '_self',
-                        ],
-                        Html::tag('i', ['class'=>$this->classMap->get('PaperButtons', 'div.editMode i')])
-                    )
+                    [
+                        Html::tag('p', [], $editor ? "Obsah edituje $editor." : '')
+                        .Html::tag('button', [
+                            'class'=>[$this->classMap->resolve($userPerformActionWithContent, 'PaperButtons', 'div.offEditMode button', 'div.editMode button'), $disabled ?? ''],
+                            'data-tooltip' => $tooltip,
+                            'name' => UserActionControler::FORM_USER_ACTION_EDIT_CONTENT,
+                            'value' => '',
+                            'type' => 'submit',
+                            'formtarget' => '_self',
+                            ],
+                            Html::tag('i', ['class'=>$this->classMap->get('PaperButtons', 'div.editMode i')])
+                        )
+                    ]
                 )
             );
     }
