5f74622cf339e1bf1b062f6047b03a403dbc885d
Kateřina Šlehoferová <slehoferova@grafia.cz>
2022-05-16 14:00:35 +0200

    styly pro šablony pro sites

--- local/site/common/templates/multipage/carousel.php
+++ local/site/common/templates/multipage/carousel.php
@@ -4,6 +4,6 @@
             <div class="carousel-wrap">
                 <?= $this->repeat(PROJECT_PATH."local/site/common/templates/paper/carousel-item.php", $pages, 'pag'); ?>
             </div>
-            <i class="huge angle right icon"></i>
+            <i class="huge angle right icon"></i> 
         </div>
     </div>
