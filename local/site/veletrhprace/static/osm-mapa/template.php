aecac6d8052bc0822ebfc7bb53fc670009ed1de0
grafiaware <grafiaware@grafia.cz>
2022-09-14 07:40:50 +0200

    osm mapa

--- local/site/veletrhprace/static/osm-mapa/template.php
+++ local/site/veletrhprace/static/osm-mapa/template.php
@@ -2,21 +2,19 @@
 // template
 use Pes\View\Renderer\PhpTemplateRendererInterface;
 use Pes\Text\Text;
+use Pes\Text\Html;
 /** @var PhpTemplateRendererInterface $this */
 
 use Site\ConfigurationCache;
-use View\Thief;
-        $urlPostfix = "index.php?lay=";
-        $urlPrefix = "http://praci.najdisi.cz/";
-        $thief = new Thief();
-        $steal = $thief->steal($urlPrefix, $urlPostfix);
 
-        $iframe =         '<iframe id="inlineFrameExample"
-    title="Inline Frame Example"
-    width="300"
-    height="200"
-    src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
-</iframe>
+//https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
+
+    $iframeId = 'inlineFrameExample';
+    $iframe = '
+        title="Inline Frame Example"
+        width="300"
+        height="200"
+        src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
         ';
 
 
@@ -28,7 +26,7 @@ use View\Thief;
                 <div class="ui grid">
                     <div class="row">
                         <div class="sixteen wide column">
-                            <iframe  src="about:blank" frameborder="0" id="steal" width=100% frameborder="0" marginheight="0" marginwidth="0">
+                            <iframe id="<?=$iframeId?>"  src="about:blank" frameborder="0" width=100% frameborder="0" marginheight="0" marginwidth="0">
 <?= $iframe ?>
                             </iframe>
 <script>
