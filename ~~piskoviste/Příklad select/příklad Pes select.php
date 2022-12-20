a7c904621c57dcaacc2b9b19f104348342642fe6
grafiaware <grafiaware@grafia.cz>
2022-12-20 13:39:26 +0100

    příklad pes select

--- ~~piskoviste/Příklad select/příklad Pes select.php
+++ ~~piskoviste/Příklad select/příklad Pes select.php
@@ -1,3 +1,8 @@
 <?php
 
-Html::select($prefixDotaznik."z_up", "Vysílající úřad práce:", ["", "Plzeň-město", "Plzeň-jih", "Plzeň-sever", "Klatovy", "Cheb", "jiné"], $poleDotaznik, [])
+        $select = Html::select("jmeno", "To je label:", ["", "Plzeň-město", "Plzeň-jih", "Plzeň-sever", "Klatovy", "Cheb", "jiné"], [], []);
+
+        $select = Html::select("jmeno", "To je label:", [1=>"", 2=>"Plzeň-město", 3=>"Plzeň-jih", 4=>"Plzeň-sever", 5=>"Klatovy", 6=>"Cheb", 7=>"jiné"], [], []);
+        $select = Html::select("jmeno", "To je label:", [1=>"", 2=>"Plzeň-město", 3=>"Plzeň-jih", 4=>"Plzeň-sever", 5=>"Klatovy", 6=>"Cheb", 7=>"jiné"], ["jmeno"=>"Plzeň-sever"], ["data-testovaci"=>"testovaci_atribut"]);
+
+        $select = Html::select("jmeno", "To je label:", [1=>"", 2=>"Plzeň-město", 3=>"Plzeň-jih", 4=>"Plzeň-sever", 5=>"Klatovy", 6=>"Cheb", 7=>"jiné"], ["jmeno"=>"nesmysl"], ["data-testovaci"=>"testovaci_atribut"]);
