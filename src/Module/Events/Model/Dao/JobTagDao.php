0ddc6514669fc2ba0eb97c50c7163d0d95c39e1f
Vladislava Selnerová <selnerova@grafia.cz>
2023-04-04 17:07:48 +0200

    po svobodovo slepení verzí

--- src/Module/Events/Model/Dao/JobTagDao.php
+++ src/Module/Events/Model/Dao/JobTagDao.php
@@ -5,14 +5,14 @@ use Model\Dao\DaoEditAbstract;
 use Model\Dao\DaoEditKeyDbVerifiedInterface;
 
 use Events\Model\Dao\JobTagDaoInterface;
-
+use \Model\Dao\DaoAutoincrementTrait;
 
 /**
  * Description of JobTagDao
  *
  * @author vlse2610
  */
-class JobTagDao    extends DaoEditAbstract  implements JobTagDaoInterface /*DaoEditKeyDbVerifiedInterface */{   
+class JobTagDao    extends DaoEditAbstract  implements DaoEditKeyDbVerifiedInterface {   
     
     use DaoAutoincrementTrait;
 
