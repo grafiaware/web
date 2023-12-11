0c08e85bd2d8865345329c7f39051c572c014a9f
pes2704 <pes2704@grafia.cz>
2023-12-11 10:57:56 +0100

    Build - upg hierarchy_view -> v3

--- src/Module/Build/Middleware/Build/Sql/makeAndConvert/page6_createHierarchy_view.sql
+++ src/Module/Build/Middleware/Build/Sql/makeAndConvert/page6_createHierarchy_view.sql
@@ -1,14 +1,34 @@
 DROP VIEW IF EXISTS hierarchy_view;
 
-CREATE VIEW `hierarchy_view` AS
-select `uid`, `left_node`, `right_node`, `parent_uid`,
-`lang_code_fk`AS lang_code,
- `uid_fk`,
- `type_fk`,
- `id`,
- `list`,
- `order`,
- `title`,
- `active`,
-`auto_generated`
+-- CREATE VIEW `hierarchy_view` AS
+-- select `uid`, `left_node`, `right_node`, `parent_uid`,
+-- `lang_code_fk`AS lang_code,
+--  `uid_fk`,
+--  `type_fk`,
+--  `id`,
+--  `list`,
+--  `order`,
+--  `title`,
+--  `active`,
+-- `auto_generated`
+-- from (`hierarchy` left join `menu_item` on((`hierarchy`.`uid` = `menu_item`.`uid_fk`)));
+
+-- TOTO JE NOVÁ VERZE - VERZE V3
+
+CREATE VIEW `hierarchy_view` AS 
+select 
+`hierarchy`.`uid` AS `uid`,
+`hierarchy`.`left_node` AS `left_node`,
+`hierarchy`.`right_node` AS `right_node`,
+`hierarchy`.`parent_uid` AS `parent_uid`,
+`menu_item`.`lang_code_fk` AS `lang_code`,
+`menu_item`.`uid_fk` AS `uid_fk`,
+`menu_item`.`id` AS `id`,
+`menu_item`.`api_module_fk` AS `api_module_fk`,
+`menu_item`.`api_generator_fk` AS `api_generator_fk`,
+`menu_item`.`list` AS `list`,
+`menu_item`.`order` AS `order`,
+`menu_item`.`title` AS `title`,
+`menu_item`.`active` AS `active`,
+`menu_item`.`auto_generated` AS `auto_generated` 
 from (`hierarchy` left join `menu_item` on((`hierarchy`.`uid` = `menu_item`.`uid_fk`)));
