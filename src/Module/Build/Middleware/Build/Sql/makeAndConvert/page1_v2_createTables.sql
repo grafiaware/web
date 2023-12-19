f04140dd9408b97e3e06f1e0de4b254fe6daf527
vlse2610 <vlse2610@grafia.cz>
2023-12-19 16:59:04 +0100

    vcetne Najdisi 2, build vsetne sql 12

--- src/Module/Build/Middleware/Build/Sql/makeAndConvert/page1_v2_createTables.sql
+++ src/Module/Build/Middleware/Build/Sql/makeAndConvert/page1_v2_createTables.sql
@@ -155,20 +155,51 @@ CREATE TABLE `menu_item` (
 
 
 
--- ----------------------------
--- Table structure for menu_item_asset
--- ----------------------------
-DROP TABLE IF EXISTS `menu_item_asset`;
-CREATE TABLE `menu_item_asset` (
-  `menu_item_id_FK` int(11) unsigned NOT NULL,
+-- -- ----------------------------
+-- -- Table structure for menu_item_asset
+-- -- ----------------------------
+-- DROP TABLE IF EXISTS `menu_item_asset`;
+-- CREATE TABLE `menu_item_asset` (
+--   `menu_item_id_FK` int(11) unsigned NOT NULL,
+--   `filepath` varchar(255) NOT NULL,
+--   `mime_type` varchar(255) NOT NULL,
+--   `editor_login_name` varchar(45) DEFAULT NULL,
+--   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   `updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+--   PRIMARY KEY (`menu_item_id_FK`,`filepath`),
+--   CONSTRAINT `menu_item_fk1` FOREIGN KEY (`menu_item_id_FK`) REFERENCES `menu_item` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
+DROP TABLE IF EXISTS `asset`;
+CREATE TABLE `asset` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `filepath` varchar(255) NOT NULL,
   `mime_type` varchar(255) NOT NULL,
   `editor_login_name` varchar(45) DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`menu_item_id_FK`,`filepath`),
-  CONSTRAINT `menu_item_fk1` FOREIGN KEY (`menu_item_id_FK`) REFERENCES `menu_item` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `filepath_idx1` (`filepath`)
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `menu_item_asset`;
+CREATE TABLE `menu_item_asset` (
+  `menu_item_id_fk` int(11) unsigned NOT NULL,
+  `asset_id_fk` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`menu_item_id_fk`,`asset_id_fk`),
+  KEY `asset_ibfk2_idx` (`asset_id_fk`),
+  CONSTRAINT `asset_ibfk2` FOREIGN KEY (`asset_id_fk`) REFERENCES `asset` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `menu_item_ibfk1` FOREIGN KEY (`menu_item_id_fk`) REFERENCES `menu_item` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
 
 -- ----------------------------
 -- Table structure for menu_item_test
