a5b704b050e95d67f6942004d73db2d62f056298
Kateřina Šlehoferová <slehoferova@grafia.cz>
2024-02-21 11:29:12 +0100

    styly pracinajdisi (ocisteno od veletrhprace)

--- local/site/common/templates/red/paper/columns_perex_contents.php
+++ local/site/common/templates/red/paper/columns_perex_contents.php
@@ -9,21 +9,31 @@ use Red\Model\Entity\PaperAggregatePaperSectionInterface;
 /** @var PaperAggregatePaperSectionInterface $paperAggregate */
 
 ?>
-
+    <!-- horizontalne-prohodit** -->
     <div class="" data-template="columns_perex_contents">
         <div class="ui grid">
             <div class="sixteen wide column">
                 <?= $headline ?>
             </div>
         </div>
-        <div class="ui grid stackable horizontalne-prohodit">
-            <div class="eight wide column">
+        <div class="ui grid horizontalne-prohodit">
+            <div class="sixteen wide mobile eight wide computer column">
                 <?= $perex ?>
             </div>
-            <div class="eight wide column">
+            <div class="sixteen wide mobile eight wide computer column">
                 <div class="ui grid">
                     <?= $this->repeat(__DIR__."/section/columns_perex_contents.php", $sections, 'paperSection'); ?>
                 </div>
             </div>
         </div>
+<!--        <div class="ui grid stackable horizontalne-prohodit">
+            <div class="eight wide column">
+                <?php /*= $perex */?>
+            </div>
+            <div class="eight wide column">
+                <div class="ui grid">
+                    <?php /*= $this->repeat(__DIR__."/section/columns_perex_contents.php", $sections, 'paperSection'); */?>
+                </div>
+            </div>
+        </div>-->
     </div>
