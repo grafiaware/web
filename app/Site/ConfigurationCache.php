9c4d470a6cea954cc8d2964765cb4ed2def31b38
vlse2610 <vlse2610@grafia.cz>
2024-03-04 16:06:35 +0100

    ConfigurationDb - Grafia -pridano: build.config.convert.items 2

--- app/Site/ConfigurationCache.php
+++ app/Site/ConfigurationCache.php
@@ -12,11 +12,11 @@ namespace Site;
 //use Site\G2 as Siteconfig;
 //const SITE_PATH = 'app/Site/G2/';
 
-//use Site\Grafia as Siteconfig;
-//const SITE_PATH = 'app/Site/Grafia/';
+use Site\Grafia as Siteconfig;
+const SITE_PATH = 'app/Site/Grafia/';
 
-use Site\NajdiSi as Siteconfig;
-const SITE_PATH = 'app/Site/NajdiSi/';
+//use Site\NajdiSi as Siteconfig;
+//const SITE_PATH = 'app/Site/NajdiSi/';
 
 //use Site\OtevreneAteliery as Siteconfig;
 //const SITE_PATH = 'app/Site/OtevreneAteliery/';
