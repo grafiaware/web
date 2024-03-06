08fe5f24c62c0a07bd3e4ca2aa04cb03b2afa917
vlse2610 <vlse2610@grafia.cz>
2024-03-06 17:29:34 +0100

    vertical.svisle withRootItem - true,

--- app/Site/OtevreneAteliery/ConfigurationWeb.php
+++ app/Site/OtevreneAteliery/ConfigurationWeb.php
@@ -229,7 +229,7 @@ class ConfigurationWeb extends ConfigurationConstants {
             'menu.componentsServices' => [
                     'menu.svisle' => [
                         'rootName' => 'menu_vertical',
-                        'withRootItem' => false,
+                        'withRootItem' => true,
                         'itemtype' => ItemRenderTypeEnum::MULTILEVEL,
                         'levelRenderer' => 'menu.svisle.levelRenderer',
                         ],
