30f7d3d09ac52410a1fd3c76fa08cca2e66379c3
Kateřina Šlehoferová <slehoferova@grafia.cz>
2024-06-14 14:59:24 +0200

    nové staré oa vzniklé z najdisi čistka

--- local/site/otevreneateliery/templates/layout/head/css.php
+++ local/site/otevreneateliery/templates/layout/head/css.php
@@ -5,7 +5,7 @@ use Pes\View\Renderer\PhpTemplateFunctionsInterface;
 <link rel="stylesheet" type="text/css" href="<?= $linksSite ?>semantic-ui/semantic.min.css" />
     <link rel="stylesheet" type="text/css" href="<?= $linksCommon ?>css/layout.css" />
     <link rel="stylesheet" type="text/css" href="<?= $linksCommon ?>css/templates.css" />
-    <link rel="stylesheet" type="text/css" href="<?= $linksCommon.'css/old/styles.css'?>" />
+    <!--<link rel="stylesheet" type="text/css" href="<?php/* echo $linksCommon.'css/old/styles.css' */?>" />-->
     <link rel="stylesheet" type="text/css" href="<?= $linksCommon.'css/media.css'?>" />
 
     <?= $this->insertIf( !$isEditableMode, __DIR__.'/cssNoneditableMode.php', ['linksCommon'=>$linksCommon] ); ?>
