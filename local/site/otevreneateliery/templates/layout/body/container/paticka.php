aa98b8364b3f9c5c2f58d77fc4b0855c3b48012f
Kateřina Šlehoferová <slehoferova@grafia.cz>
2024-07-18 12:02:44 +0200

    footer

--- local/site/otevreneateliery/templates/layout/body/container/paticka.php
+++ local/site/otevreneateliery/templates/layout/body/container/paticka.php
@@ -1,10 +1,8 @@
                 <div class="ui grid">
                     <div class="fifteen wide column">
-                        <div class="loga-paticka">
-                            <div class="blok-menu-paticka">
-                                <p class="text velky tucne primarni-barva">Organizátor otevřených ateliérů</p>
-                                <a href="https://www.grafia.cz/" class="" target="_blank"><img class="organizator" src="layout-images/Grafia_logo.jpg" width="" height="" alt="Logo Grafia, organizátor"/></a>
-                            </div>
+                        <div class="blok-menu-paticka">
+                            <p class="text velky tucne primarni-barva">Organizátor otevřených ateliérů</p>
+                            <a href="https://www.grafia.cz/" class="" target="_blank"><img class="organizator" src="layout-images/grafia_logo.gif" width="" height="70" alt="Logo Grafia, organizátor"/></a>
                         </div>
                     </div>
                 </div>
