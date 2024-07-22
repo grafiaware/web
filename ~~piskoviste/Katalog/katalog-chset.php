18ddb86e8b1c4af55756d192ae3e765a306b44a5
pes2704 <pes2704@grafia.cz>
2024-07-22 21:21:49 +0200

    OAKatalogGenerator - test

---
+++ ~~piskoviste/Katalog/katalog-chset.php
@@ -0,0 +1,18 @@
+<?php
+use Pes\View\Renderer\PhpTemplateRendererInterface;
+use Pes\Text\Text;
+use Pes\Text\Html;
+
+/** @var PhpTemplateRendererInterface $this */
+
+?>
+
+ 
+<?=
+        "<a href=\"web/v1/page/item/$thisSite#$chNazev\">$chNazev</a>|"       
+            /* <a href="web/v1/page/item/66681f58db5e9#A"> A </a>          */
+        
+?>
+ 
+
+
