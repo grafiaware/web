a4cd1b371686cc49e60ea16c26c545b1ae25a801
vlse2610 <vlse2610@grafia.cz>
2024-07-22 19:27:39 +0200

    katalog umelcu - template php 4

--- local/site/otevreneateliery/static/katalog/katalog-blockset-row.php
+++ local/site/otevreneateliery/static/katalog/katalog-blockset-row.php
@@ -8,14 +8,24 @@ use Pes\Text\Html;
 ?>
 
 <div> 
+    
     <?= 
-
-        "- $prijmeni $jmeno"
+        
+            isset($nazevInstituce) ?
+               "- $prijmeni $jmeno - " .
+               "<a href=\"web/v1/page/item/$site#$webAnchor\">$prijmeni $jmeno</a></p>"
+            :
+               "- $prijmeni $jmeno - " .
+               "<a href=\"web/v1/page/item/$site#$webAnchor\">$prijmeni $jmeno</a></p>"
+                  
+        /* web anchor neni ????  nedelat odkaz*/
+                
     ?> 
 
     
 <!--<p id="A" style="margin-top: 30px;"><strong>A</strong></p>
-<p style="margin-top: 5px;"><a href="web/v1/page/item/6671583cb7df7#ambasador-atelier-markety-pangrac">Ambasador atelier Mark&eacute;ty Pangr&aacute;c</a></p>-->   
+<p style="margin-top: 5px;">
+<a href="web/v1/page/item/6671583cb7df7#ambasador-atelier-markety-pangrac">Ambasador atelier Mark&eacute;ty Pangr&aacute;c</a></p>-->   
 
  </div> 
 
