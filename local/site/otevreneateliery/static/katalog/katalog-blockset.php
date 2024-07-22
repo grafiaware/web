ccb288973dd4d326b3393b7ff09ccc91f476d066
vlse2610 <vlse2610@grafia.cz>
2024-07-22 16:24:58 +0200

    katalog umelcu - template php 3

--- local/site/otevreneateliery/static/katalog/katalog-blockset.php
+++ local/site/otevreneateliery/static/katalog/katalog-blockset.php
@@ -7,17 +7,12 @@ use Pes\Text\Html;
 
 ?>
 
-<div> 
+      
+<?=  $pismeno   ?>
+
 <?= 
-  /*  "<a href=\"web/v1/page/item/$thisSite#$chNazev\">$chNazev</a>|<br>" */
-    
-                <?= $this->insert( __DIR__.'/katalog-blockset-row.php' ) ?>                                                                                 
+     $this->repeat(__DIR__.'/katalog-blockset-row.php',  $klienti) 
+?>
    
-/* 
 <!--<p id="A" style="margin-top: 30px;"><strong>A</strong></p>
 <p style="margin-top: 5px;"><a href="web/v1/page/item/6671583cb7df7#ambasador-atelier-markety-pangrac">Ambasador atelier Mark&eacute;ty Pangr&aacute;c</a></p>-->
- */
-        
-?>
- </div> 
-
--- local/site/otevreneateliery/static/katalog/katalog-chset.php
+++ local/site/otevreneateliery/static/katalog/katalog-chset.php
@@ -7,12 +7,12 @@ use Pes\Text\Html;
 
 ?>
 
-<div> 
-<?= "<a href=\"web/v1/page/item/$thisSite#$chNazev\">$chNazev</a>|<br>"
-
-/* <a href="web/v1/page/item/66681f58db5e9#A"> A </a>          */
+ 
+<?=
+        "<a href=\"web/v1/page/item/$thisSite#$chNazev\">$chNazev</a>|"       
+            /* <a href="web/v1/page/item/66681f58db5e9#A"> A </a>          */
         
 ?>
-</div> 
+ 
 
 
