c316be56ae0562f7fce9a41530a117b51c94a7b5
vlse2610 <vlse2610@grafia.cz>
2024-08-01 16:41:44 +0200

    test movesection

--- tests/Integration/Red/Model/Context/ContextProviderMock.php
+++ tests/Integration/Red/Model/Context/ContextProviderMock.php
@@ -17,8 +17,7 @@ use Model\Context\ContextProviderInterface;
  */
 class ContextProviderMock implements ContextProviderInterface {
 
-    private $editableMode;
-    private $actual;
+    private $forceShowOnlyPublished;
 
     public function __construct($editableMode=false) {
         $this->editableMode = (bool) $editableMode;
@@ -28,18 +27,19 @@ class ContextProviderMock implements ContextProviderInterface {
      * Metoda slouží pro změnu nastavení, které se v produkčních objektech contect provider provádí výhradne pomocí parametrů konstruktoru.
      *
      * Tento mock lze nastavovat, je jen třeba dbát na to, aby nastavení proběhlo před prvním čtením.
-     *
-     * @param type $editableMode
+     * @param type $forceShowOnlyPublished
+     * @return void
      */
-    public function changeContext($editableMode=false) {
-        $this->editableMode = (bool) $editableMode;
+    public function forceShowOnlyPublished($forceShowOnlyPublished=null): void {
+        $this->forceShowOnlyPublished = $forceShowOnlyPublished;
     }
-
-    /**
-     * Produkční PublishedContext odvozuje kontext z presentation statusu
-     * @return bool
-     */
+    
     public function showOnlyPublished(): bool {
-        return ! $this->editableMode;
+        if (isset($this->forceShowOnlyPublished)) {
+            $show = (bool) $this->forceShowOnlyPublished;
+        } else {
+            $show = true;
+        }
+        return $show;
     }
 }
