33b7cea75ee44e5c5fac1ee629d7c8c016a1eb17
Kateřina Šlehoferová <slehoferova@grafia.cz>
2024-08-02 15:21:40 +0200

    Šablona Umělec a Umělec_vnořený, nutné zařadit do nabídky pro Tiny

---
+++ local/site/common/templates/red/author/umelec.php
@@ -0,0 +1,7 @@
+<div class="blok_text_obrazek" data-template="<?= "umelec" ?>">
+    <h4><a id="zmente-kotvu"></a>Příjmení jméno, nezapomeňte na kotvu!</h4>
+    <p>text</p>
+    <div class="obrazky_inline">
+        zde vložte obrázky do max. šířky 800px 
+    </div>
+</div>
---
+++ local/site/common/templates/red/author/umelec_vnoreny.php
@@ -0,0 +1,7 @@
+<div class="blok_text_obrazek vnor" style="margin-left: 40px;" data-template="<?= "umelec_vnoreny" ?>">
+    <h4><a id="zmente-kotvu"></a>Příjmení jméno, nezapomeňte na kotvu!</h4>
+    <p>text</p>
+    <div class="obrazky_inline">
+        zde vložte obrázky do max. šířky 700px 
+    </div>
+</div>
