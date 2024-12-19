9da3dc216906b21ee60145ab3900b387ac89b83d
vlse2610 <vlse2610@grafia.cz>
2024-12-19 17:20:48 +0100

    proces zapomenute heslo - ok, tab. credentials -  neuklada!

--- src/Module/Auth/Middleware/Login/Controler/Messages/confirm.php
+++ src/Module/Auth/Middleware/Login/Controler/Messages/confirm.php
@@ -11,7 +11,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>veletrhPRACE.online</title>
+        <title></title>
     </head>
     <body  style="margin: 0; padding: 0;">
         <table role="presentation"  cellspacing="0" cellpadding="0" border="0" style="border-collapse: collapse;">
