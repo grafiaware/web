272abf6f640d449148b76dd18665a455d7654785
pes2704 <pes2704@grafia.cz>
2025-01-02 09:10:26 +0100

    url encode fix

--- local/site/najdisi/static/admin-role/role.php
+++ local/site/najdisi/static/admin-role/role.php
@@ -5,13 +5,13 @@
             <?php  if (isset ($role) ) {   ?>
                     <input  type="text" name="role" placeholder="" maxlength="50" 
                             pattern="[a-z]+"   title="Jen malá písmena - bez háčků a čárek" value="<?= $role ?>">
-                    <button class='ui secondery button' type='submit' formaction='<?= rawurlencode("auth/v1/role/$role") ?>'>Ulozit</button>
-                    <button class='ui primary button' type='submit' formaction='<?= rawurlencode("auth/v1/role/$role/remove") ?>'>Odstranit</button>
+                    <button class='ui secondery button' type='submit' formaction='<?= "auth/v1/role/$role" ?>'>Ulozit</button>
+                    <button class='ui primary button' type='submit' formaction='<?= "auth/v1/role/$role/remove" ?>'>Odstranit</button>
             <?php  } else {   ?>    
                     <input  type="text" name="role" placeholder="Zadejte jednoslovný název role složený z alfanumerických znaků" maxlength="50" 
                             pattern="[a-z]+"   title="Jen malá písmena - bez háčků a čárek" 
                             required>
-                    <button class='ui secondery button' type='submit' formaction='<?= rawurlencode("auth/v1/role") ?>'>Uložit</button>             
+                    <button class='ui secondery button' type='submit' formaction='<?= "auth/v1/role" ?>'>Uložit</button>             
             <?php  } ?>            
         </div>
     </form >
