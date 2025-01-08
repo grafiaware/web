73f91693a8944b2abf8a72c129e4a6bc01c67af9
pes2704 <pes2704@grafia.cz>
2025-01-08 16:42:21 +0100

    textarea do user unput

--- local/site/najdisi/templates/events/data/company-info/info-editable.php
+++ local/site/najdisi/templates/events/data/company-info/info-editable.php
@@ -10,21 +10,21 @@ use Pes\Text\Html;
 ?>
             <div class="field">
                 <label>O nás</label>
-                <input class="edit-userinput" type="text" name="introduction" placeholder="" maxlength="1000"><?= $introduction ?? '' ?></textarea>
+                <textarea class="edit-userinput" type="text" name="introduction" placeholder="" maxlength="1000"><?= $introduction ?? '' ?></textarea>
             </div>
             <div class="field">
                 <label>Video</label>
-                <input id="youtubeUrl" type="text" name="videolink" placeholder="https://www.youtube.com/watch?v=..." maxlength="150"><?= $videoLink ?? '' ?></textarea>
+                <textarea id="youtubeUrl" type="text" name="videolink" placeholder="https://www.youtube.com/watch?v=..." maxlength="150"><?= $videoLink ?? '' ?></textarea>
                 <p id="message"></p>
             </div>
             <div class="two fields">
                 <div class="field">
                     <label>Proč k nám</label>
-                    <input class="edit-userinput" type="text" name="positives" placeholder="" maxlength="1000"><?= $positives ?? '' ?></textarea>
+                    <textarea class="edit-userinput" type="text" name="positives" placeholder="" maxlength="1000"><?= $positives ?? '' ?></textarea>
                 </div>
                 <div class="field">
                     <label>Jak žijeme</label>
-                    <input class="edit-userinput" type="text" name="social" placeholder="" maxlength="1000"><?= $social ?? '' ?></textarea>
+                    <textarea class="edit-userinput" type="text" name="social" placeholder="" maxlength="1000"><?= $social ?? '' ?></textarea>
                 </div>
             </div>             
 
