b43ac47c2c15c68424567a85c0474b3559617885
pes2704 <pes2704@grafia.cz>
2025-01-19 16:59:23 +0100

    templ

--- local/site/najdisi/templates/events/data/network/network-editable.php
+++ local/site/najdisi/templates/events/data/network/network-editable.php
@@ -14,9 +14,9 @@
 
 <!--<div class="two fields">   
         <div class="field">
-            <input type="text" name="icon" placeholder="název ikony" maxlength="28" required value="<?= $icon ?? '' ?>">
-        </div>
-        <div class="field">                
+            <input type="text" name="icon" plac
+        <div class="fieldeholder="název ikony" maxlength="28" required value="<?= $icon ?? '' ?>">
+        </div>">                
             <input type="text" name="embedCodeTemplate" placeholder="vložte kód embedCodeTemplate" maxlength="120" required value="<?= $embedCodeTemplate ?? '' ?>">              
         </div>
     </div>-->
