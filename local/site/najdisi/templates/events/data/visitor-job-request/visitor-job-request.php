19259e44468efba4995ea5c3a7b54c4c996235a3
Kateřina Šlehoferová <slehoferova@grafia.cz>
2025-01-28 14:35:58 +0100

    uprava tabulka údajů,co navstevnik odeslal

--- local/site/najdisi/templates/events/data/visitor-job-request/visitor-job-request.php
+++ local/site/najdisi/templates/events/data/visitor-job-request/visitor-job-request.php
@@ -10,40 +10,36 @@ use Pes\View\Renderer\PhpTemplateRendererInterface;
            <div class="ui internally celled grid">
                <div class="row">
                     <div class="sixteen wide column">
-                            <p  class="text primarni-barva zadne-okraje"><?php echo (isset($prefix) ? "$prefix " : "").(isset($name) ? "$name " : "").(isset($surname) ? "$surname" : "").(isset($postfix) ? ", $postfix" : "") ?></p>
+                            <p  class="text velky primarni-barva zadne-okraje"><?php echo (isset($prefix) ? "$prefix " : "").(isset($name) ? "$name " : "").(isset($surname) ? "$surname" : "").(isset($postfix) ? ", $postfix" : "") ?></p>
                     </div>
                </div>
                <div class="row">
                     <div class="eight wide column">
-                        E-mail
+                        <b>E-mail</b>
+                        <div>
+                            <?= $email ?? '';  ?>
+                        </div>
                     </div>
                     <div class="eight wide column">
-                        <?= $email ?? '';  ?>
+                        <b>Telefon</b>
+                        <div>
+                            <?= $phone ?? '';  ?>
+                        </div>
                     </div>
                </div>
                <div class="row">
                     <div class="eight wide column">
-                        Telefon
-                    </div>
-                    <div class="eight wide column">
-                        <?= $phone ?? '';  ?>
-                    </div>
-               </div>
-               <div class="row">
-                    <div class="sixteen wide column">
-                        <p>Vzdělání, kurzy</p>
+                        <b>Vzdělání, kurzy</b>
                         <div>
                             <?= $cvEducationText ?? ''; ?>
                         </div>
                     </div>
-               </div>
-               <div class="row">
-                    <div class="sixteen wide column">
-                        <p>Pracovní zkušenosti, dovednosti</p>
+                    <div class="eight wide column">
+                        <b>Pracovní zkušenosti, dovednosti</b>
                         <div>
                             <?= $cvSkillsText ?? ''; ?>
                         </div>
                     </div>
-                </div>
+               </div>
             </div>
                                          
