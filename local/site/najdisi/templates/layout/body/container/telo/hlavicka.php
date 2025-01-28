0795dee5ea27cd37cd4cbbed9c1554a24f22bbdc
Kateřina Šlehoferová <slehoferova@grafia.cz>
2025-01-28 14:30:27 +0100

    širší obrázek k širšímu webu

--- local/site/najdisi/templates/layout/body/container/telo/hlavicka.php
+++ local/site/najdisi/templates/layout/body/container/telo/hlavicka.php
@@ -8,7 +8,7 @@ use Pes\Text\Text;
 /** @var PaperAggregatePaperSectionInterface $paperAggregate */
 ?>
 
-            <a href="index.php" class="header-pc"><img class="logo-najdisi" src="layout-images/VPV_2025_webovy_banner.jpg" width="1332" height="367"/></a>
+            <a href="index.php" class="header-pc"><img class="logo-najdisi" src="layout-images/VPV_2025_webovy_banner2.jpg" width="100%" height="370"/></a>
             <a href="index.php" class="header-mobile"><img class="logo-najdisi-mobile" src="layout-images/VPV_2025_webovy_banner_mobile.jpg" width="auto" height="300"/></a>
             
             <div class="mobile-menu-bar" onclick="hamburger_open()"> <div class="nav-mobile active"><i class="bars big icon"></i><p>Menu</p></div></div>
