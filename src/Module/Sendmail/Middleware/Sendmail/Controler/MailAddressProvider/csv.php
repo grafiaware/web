6b3ef129d27b59f15e199a2af8a865a0ef0d3d71
pes2704 <pes2704@grafia.cz>
2025-02-25 14:12:52 +0100

    přidána složka pro emaily z csv

---
+++ src/Module/Sendmail/Middleware/Sendmail/Controler/MailAddressProvider/csv.php
@@ -0,0 +1,42 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+function importCsv($filename) {
+    $file = new SplFileObject($filename, "r");
+    $file->setFlags(SplFileObject::READ_CSV);
+    $first = true;
+    foreach ($file as $row) {
+        if ($first) {
+            $headers = array_map(function($val){return iconv("Windows-1250", "UTF-8//IGNORE", $val);}, $row);
+            $first = false;
+        } else {
+            $utf8row = array_map(function($val){return iconv("Windows-1250", "UTF-8//IGNORE", $val);}, $row);
+            if ($utf8row[0]) {
+                $data[] = array_combine($headers, $utf8row); // Spojí hlavičku s daty
+            }
+        }
+    }
+    return $data;
+}
+
+function exportCsv($filename, array $data) {
+    $file = new SplFileObject($filename, "w");
+    $first = true;
+    foreach ($data as $dataRow) {
+        if ($first) {
+            $headers = array_map(function($val){return iconv("UTF-8", "Windows-1250", $val);}, $dataRow);
+            $file->fputcsv($headers);
+            $first = false;
+        } else {
+            $row = array_map(function($val){return iconv("UTF-8", "Windows-1250", $val);}, $dataRow);
+            $file->fputcsv($row);
+            
+        }
+    }
+}
+
+
+
