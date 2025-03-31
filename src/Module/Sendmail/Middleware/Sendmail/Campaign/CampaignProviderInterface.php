1e0ae11b4d12f424c90812551713bb420d783743
vlse2610 <vlse2610@grafia.cz>
2025-03-31 13:34:04 +0200

    změna honstanty CAMPAIGN - použita v routě

--- src/Module/Sendmail/Middleware/Sendmail/Campaign/CampaignProviderInterface.php
+++ src/Module/Sendmail/Middleware/Sendmail/Campaign/CampaignProviderInterface.php
@@ -6,8 +6,9 @@ namespace Sendmail\Middleware\Sendmail\Campaign;
  * @author pes2704
  */
 interface CampaignProviderInterface {
-    
-    const CAMPAIGN_ANKETY_2025 = "anketni lístky návštěvníků";    
+    // hondota (nikoli jméno) konstanty je použitajako součást URL routy proto hodnota nesmí obsahovat mezery (musí to být "jedno" slovo) 
+    // a nesmí obsahovat lomítka
+    const CAMPAIGN_ANKETY_2025 = "anketni_listky_navstevniku_2025";    
     
     public function getCampaignConfig($name);
 }
