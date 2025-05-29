a56f9ebc46902055eda4831666a8c9cd495658be
Kateřina Šlehoferová <slehoferova@grafia.cz>
2025-05-29 14:41:21 +0200

    přihlášení s errorama, styly

--- local/site/otevreneateliery/templates/layout/body/container/telo/prihlaseni.php
+++ local/site/otevreneateliery/templates/layout/body/container/telo/prihlaseni.php
@@ -1,14 +1,18 @@
-        <div class="prihlaseni">
+<?php
+use Pes\Text\Html;
+?>
+
+<div class="prihlaseni <!--schvalene-->">
             <?=
-                $modalLogin ?? '';
-            ?>
-            <?=
-                $modalLogout ?? '';
-            ?>
-            <?=
-                $modalRegister ?? '';
-            ?>
-            <?=
-                $modalUserAction ?? ''
+                Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"auth/v1/component/login",]) 
+                . 
+                Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"auth/v1/component/logout",]) 
+                . 
+                Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"auth/v1/component/register",]) 
+                . 
+                Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"red/v1/component/presentationAction",]) 
+                .
+                Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"events/v1/component/representativeAction",])
+        ;
             ?>
         </div>
