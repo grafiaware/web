5f6a40e356d9588c41e624e6ebb238a4a6fbc5f6
pes2704 <pes2704@grafia.cz>
2026-01-04 14:19:21 +0100

    přusunuty static templates

--- local/site/najdisi/static/auth/dokonceni-registrace/template.php
+++ local/site/najdisi/static/auth/dokonceni-registrace/template.php
@@ -3,4 +3,4 @@ use Site\ConfigurationCache;
 ?>
 <h3> Veletrh práce a vzdělávání </h3>
 <p> Vaše registrace byla dokončena. Děkujeme Vám za spolupráci.  </p> 
-<p>Pro přihlášení použijte přihlašovací ikonu nalevo.</p>
+<p>Pro přihlášení použijte přihlašovací ikonu.</p>
