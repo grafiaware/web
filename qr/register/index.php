8b9c9f8d9251899b80591c8c8de2a9468d83bfa6
pes2704 <pes2704@grafia.cz>
2026-01-12 11:15:44 +0100

    qr přesměrování

--- qr/register/index.php
+++ qr/register/index.php
@@ -5,5 +5,5 @@
  */
 
 //http_response_code(302)  - temporary redirect 
-header('Location: https://praci.najdisi.cz/web/v1/page/item/65aff921a7caa', true, '302');
+header('Location: https://praci.najdisi.cz/?modal=register0AXR2026', true, '302');
 exit;
