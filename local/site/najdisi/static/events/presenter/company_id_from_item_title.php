92d9cdac340a314e19aeb1a89624b58ebd7f42dd
pes2704 <pes2704@grafia.cz>
2026-01-23 23:00:55 +0100

    změna company versionn

--- local/site/najdisi/static/events/presenter/company_id_from_item_title.php
+++ local/site/najdisi/static/events/presenter/company_id_from_item_title.php
@@ -27,8 +27,6 @@ $itemTitle = $statusViewModel->getPresentedMenuItem()->getTitle();
 /** @var CompanyRepoInterface $companyRepo */
 $companyRepo = $container->get(CompanyRepo::class);
 $company = $companyRepo->find(" name LIKE :name_like AND version_fk=:version_fk", [":name_like"=>"%$itemTitle%", "version_fk"=>$version_fk]);
-$msg = print_r($company, true);
-echo "<code>$msg</code>";
 $cnt = count($company);
 if ($cnt==0) {
     echo "<p>Chyba: Nenalezena company podle textu položky menu '$itemTitle' s verzí '$version_fk'.</p>";
