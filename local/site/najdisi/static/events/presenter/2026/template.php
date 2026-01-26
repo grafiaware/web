43af6b43892dbe1058bc587b820b21893809da64
pes2704 <pes2704@grafia.cz>
2026-01-26 10:52:47 +0100

    PROVIZORN9 ŘEŠENÍ - base do pdf template

--- local/site/najdisi/static/events/presenter/2026/template.php
+++ local/site/najdisi/static/events/presenter/2026/template.php
@@ -12,10 +12,10 @@ require __DIR__.'/../company_id_from_item_title.php';
 include ConfigurationCache::eventTemplates()['templates']."presenter/company-profile.php";
 
 // proměnné pro files/pdf/template.php
-$path = ConfigurationCache::files()['@presenter']."/smycka/$yearPath/";
+$filePath = ConfigurationCache::files()['@presenter']."/smycka/$yearPath/";   // cesta k souborům - ne path
 $file = "$companyName.pdf";
 
 // $path, $file - proměnná pro files/pdf/template.php
-if (is_readable($path.$file)) {
+if (is_readable($filePath.$file)) {
     include ConfigurationCache::commonTemplates()['templates']."files/pdf/template.php";
 }
