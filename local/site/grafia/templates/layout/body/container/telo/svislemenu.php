d7f124a37b0c591bb03269db9e039f8007d3163b
pes2704 <pes2704@grafia.cz>
2026-02-23 08:31:40 +0100

    cs-fixer

--- local/site/grafia/templates/layout/body/container/telo/svislemenu.php
+++ local/site/grafia/templates/layout/body/container/telo/svislemenu.php
@@ -3,18 +3,21 @@ use Pes\Text\Html;
 ?>
         <div id="mySidenav">
             <div class="close-item" onclick="hamburger_close()"><a href="javascript:void(0)"><i class="times circle outline large icon"></i>Zavřít</a></div>
-
-            <nav class="svisle-menu bloky">
-                <?= $bloky ?? ''?>
-            </nav>
-            <nav class="svisle-menu hlavni-menu">
-                <?= $menuVertical ?? ''?>
-            </nav>
+         
+            <nav class="svisle-menu">
+                <?= Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"red/v1/component/menuRedirect",])  ?>
+            </nav>            
+            <nav class="svisle-menu">
+                <?= Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"red/v1/component/menuHorizontal",])  ?>
+            </nav>            
             <nav class="svisle-menu">
                 <?= Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"red/v1/component/menuVertical",])  ?>
-            </nav>            
+            </nav>
             <nav class="svisle-menu kos">
-                <?= $kos ?? '' ?>
+                <?= Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"red/v1/component/menuTrash",])  ?>
+            </nav>
+            <nav class="svisle-menu bloky">
+                <?= Html::tag('div', ['class'=>'cascade', 'data-red-apiuri'=>"red/v1/component/menuBlocks",])  ?>
             </nav>
 
             <?=
