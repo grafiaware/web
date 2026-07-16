c3fa3f978c392cc645cb59cc76b0c8bed1575bbb
pes2704 <pes2704@grafia.cz>
2026-07-16 14:49:39 +0200

    fix OKKatalogGeneratorTest

--- src/Module/Events/Model/Context/ContextProvider.php
+++ src/Module/Events/Model/Context/ContextProvider.php
@@ -34,7 +34,24 @@ class ContextProvider implements ContextProviderInterface {
     public function __construct(
             ) {
     }
-
+    
+    /**
+     * Jen pokud víte co děláte!
+     * Metoda slouží pro změnu nastavení, které contect provider provádí pomocí parametrů konstruktoru.
+     *
+     * Touto metodou lze vynutit pevné nastavení, je jen třeba dbát na to, aby nastavení proběhlo před prvním čtením.
+     * 
+     * POZOR! Použití této metody může vést ke komplikovaným důsledkům. Například pokud se ContextProvider používá v kontejneru, pak je s největší pravděpodobností použit 
+     * v různých Dao a Repository a změna jeho nastavé ovlivní všechny. Navíc až od okamžiku nastavení. 
+     * Lze to snad eliminovat voláním metody s bool hodnotou, provést operaci s jedním Dao (Repo - pozor na agregátní repo) a ihned zavolat metodu s hodnotou parametru null.
+     * Zavolánim metody s hodnotou null se vrátí chování objektu k defaultnímu.
+     * 
+     * @param bool $forceShowOnlyPublished
+     */
+    public function forceShowOnlyPublished($forceShowOnlyPublished=null): void {
+        $this->forceShowOnlyPublished = $forceShowOnlyPublished;
+    }
+    
     //TODO: Context factory pro events vrací showOnlyPublished() vždy true - vývojová verze - dodělat
     public function showOnlyPublished(): bool {
         return true;
