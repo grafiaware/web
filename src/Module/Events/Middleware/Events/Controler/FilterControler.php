9c497973029fdf5e20c851bc27aa35deb3f62646
pes2704 <pes2704@grafia.cz>
2026-07-31 15:37:56 +0200

    fix FilteControler languageCode()

--- src/Module/Events/Middleware/Events/Controler/FilterControler.php
+++ src/Module/Events/Middleware/Events/Controler/FilterControler.php
@@ -44,8 +44,8 @@ class FilterControler extends FrontControlerAbstract {
         $tags = (new RequestParams())->getParsedBodyParam($request, self::FILTER_TAGS );  // když není žádný checkbox zaškrtnut => nejsou POST data => $data=null
         $selectCompanyId = (new RequestParams())->getParsedBodyParam($request, self::FILTER_COMPANY );   //AuthControler::NULL_VALUE;   
         
-        $statusPresentation = $this->statusPresentationRepo->get();)
-        $this->langCode = $statusPresentation->getLangCode();
+        $statusPresentation = $this->statusPresentationRepo->get();
+        $this->langCode = $statusPresentation->getLanguageCode();
         if (isset($statusPresentation) ) {
             $statusPresentation->setInfo(self::FILTER, [self::FILTER_TAGS=>$tags, self::FILTER_COMPANY=>$selectCompanyId]);  
         } else {
