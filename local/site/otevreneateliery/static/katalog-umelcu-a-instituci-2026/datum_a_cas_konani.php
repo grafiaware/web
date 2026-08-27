9f7947f195effc6d3df06b31d1f50676d81e50ab
pes2704 <pes2704@grafia.cz>
2026-08-27 14:04:56 +0200

    status->get na getCleno ve static přidán oa katalog2026

---
+++ local/site/otevreneateliery/static/katalog-umelcu-a-instituci-2026/datum_a_cas_konani.php
@@ -0,0 +1,8 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+$datumAcas = "26. - 27. září 2026, 14 – 20:00 hodin";
---
+++ local/site/otevreneateliery/static/katalog-umelcu-a-instituci-2026/katalog-chset.php
@@ -0,0 +1,14 @@
+<?php
+use Pes\View\Renderer\PhpTemplateRendererInterface;
+use Pes\Text\Text;
+use Pes\Text\Html;
+
+/** @var PhpTemplateRendererInterface $this */
+?>
+
+<?=
+        "<a href=\"#$chNazev\">$chNazev</a> |"              
+?>
+ 
+
+
